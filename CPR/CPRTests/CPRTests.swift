b1644785af20249bd3c08f3d9b7c5b61a5f868b4
Kay Lab <kaylab@dhcp-v001-096.mobile.uci.edu>
2015-11-06 20:41:34 -0800

    no message

---
+++ CPR/CPRTests/CPRTests.swift
@@ -0,0 +1,36 @@
+//
+//  CPRTests.swift
+//  CPRTests
+//
+//  Created by Kay Lab on 11/6/15.
+//  Copyright © 2015 Kay Lab. All rights reserved.
+//
+
+import XCTest
+@testable import CPR
+
+class CPRTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
